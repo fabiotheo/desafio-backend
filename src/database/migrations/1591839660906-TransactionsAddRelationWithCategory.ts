d07f33cfca3b8902cf35db13a596d2b713b91842
Fábio - IPCOM <fabio@ipcom.com.br>
2020-06-10 22:43:05 -0300

    Exercicio

---
+++ src/database/migrations/1591839660906-TransactionsAddRelationWithCategory.ts
@@ -0,0 +1,8 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class TransactionsAddRelationWithCategory1591839660906
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<any> {}
+
+  public async down(queryRunner: QueryRunner): Promise<any> {}
+}
